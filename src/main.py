2ccd12e029ca79dc1ccbb857c38f77580844c706
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 15:57:57 -0600

    añadir cors middleware y ruta raíz

--- src/main.py
+++ src/main.py
@@ -1,4 +1,30 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from .config.environment import AppEnv
+
 from src.routes import charts
 app = FastAPI(title="AI Charts API")
+
+# CORS
+origins = AppEnv.allowed_origins.split(' ')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(charts.router)
+
+
+@app.get("/")
+async def root():
+    return {
+        "message": "Bienvenido a AI Charts API!",
+        "routes": [
+            "/charts/generate",
+            "/charts/build"
+        ]
+    }
