c1b04bf0ceb776c6585b72ca73d82a87cb8cf647
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 16:26:19 -0600

    eliminar configuraciones no utilizadas

--- src/config/environment.py
+++ src/config/environment.py
@@ -7,7 +7,5 @@ load_dotenv()
 
 @dataclass(frozen=True)
 class AppEnv:
-    ai_model: str = os.environ["AI_MODEL"]
-    ai_api_url: str = os.environ["AI_API_URL"]
     simple_chart_prompt: str = os.environ["SIMPLE_CHART_PROMPT"]
     allowed_origins: str = os.environ["ALLOWED_ORIGINS"]
