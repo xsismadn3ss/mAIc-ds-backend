88dae0be5fe573c2fa90bf1b3dceb840d7f7ae7b
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 15:11:13 -0600

    aplicar formato al código

--- src/service/llm/impl/g4f_llm.py
+++ src/service/llm/impl/g4f_llm.py
@@ -3,8 +3,10 @@ from g4f import ChatCompletion
 from src.models.schema.llm_schema import LLM_Message
 from src.service.llm.abc_llm import ABC_LLM
 from typing import List, override
+
+
 class G4f_LLM(ABC_LLM):
-    def __init__(self, client = Client()) -> None:
+    def __init__(self, client=Client()) -> None:
         self.client = client
         super().__init__()
 
@@ -12,15 +14,14 @@ class G4f_LLM(ABC_LLM):
     def create(self, messages: List[LLM_Message]) -> str:
         payload = []
         for m in messages:
-            payload.append({
-                "role": m.role,
-                "content": m.content
-            })
+            payload.append({"role": m.role, "content": m.content})
 
         response: ChatCompletion = self.client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=payload
+            model="gpt-4o-mini", messages=payload
         )
-        
+
         choice: str = response.choices[0].message.content
         return choice
+
+
+g4f_llm = lambda: G4f_LLM()
