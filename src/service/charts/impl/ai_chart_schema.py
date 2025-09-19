627ae07732c61edc23b6e6cbd4f50e42535ade44
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 15:12:38 -0600

    corregir esquema de los métodos de la implementación

--- src/service/charts/impl/ai_chart_schema.py
+++ src/service/charts/impl/ai_chart_schema.py
@@ -1,32 +1,34 @@
-from src.service.charts.abc_chart_schema import ABC_Chart_Schema
+from src.service.charts.abc_chart_schema import ABC_ChartSchema
 from src.service.llm.impl.g4f_llm import G4f_LLM
 from src.service.llm.abc_llm import ABC_LLM
-from src.config.environment import App_config
+from src.config.environment import AppEnv
 from src.models.schema.llm_schema import LLM_Message
-from typing import override
+from src.models.schema.charts import ChartSchema
+from typing import override, List
 import json
 from fastapi import HTTPException
 
 
-class AI_Chart_Schema(ABC_Chart_Schema):
-    def __init__(self, llm:ABC_LLM = G4f_LLM()) -> None:
+class AI_ChartSchema(ABC_ChartSchema):
+    def __init__(self, llm: ABC_LLM = G4f_LLM()) -> None:
         self.llm = llm
         super().__init__()
 
     @override
-    def build_schemas(self, columns, dtypes, describe):
-        base_propmt: str = App_config.data_science_propmt
-
+    def build_schema(self, columns, dtypes, describe) -> List[ChartSchema]:
+        base_prompt: str = AppEnv.simple_chart_prompt
         prompt: str = (
-            f"{base_propmt}\nColumnas: {str(columns)}\n\nTipos de datos: {str(dtypes)}\n\nResumen: {str(describe)}"
+            f"{base_prompt}\nColumnas: {str(columns)}\n\nTipos de datos: {str(dtypes)}\n\nResumen: {str(describe)}"
         )
-
         msg = LLM_Message(content=prompt)
         response = self.llm.create(messages=[msg])
-        print(response)
 
         try:
-            return json.loads(response)
+            data: dict = json.loads(response)
+            return [ChartSchema(**d) for d in data.get("charts")]
         except Exception as e:
             print(type(e))
             raise HTTPException(500, e)
+
+
+ai_chart_schema = lambda: AI_ChartSchema()
