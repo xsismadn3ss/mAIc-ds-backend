8f781256b085b5f45b6a1668f3ab5571dffe28be
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 15:10:39 -0600

    crear archivo para generar pipes a tráves de un archivo

--- src/service/pipe/abstract_pipe_service.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from pandas import DataFrame
-from fastapi import UploadFile
-
-class Abstract_Pipe_Service(ABC):
-    @abstractmethod
-    async def generate_df(self, file: UploadFile) -> DataFrame:
-        raise NotImplementedError()
