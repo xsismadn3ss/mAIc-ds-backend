2e64fbec9f41c3b3f1bcf2f2bfefa82ec3aa4863
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-18 19:41:26 -0600

    implementar servicio de cliente llm

---
+++ src/service/llm/abc_llm.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from typing import List
+from src.models.schema.llm_schema import LLM_Message
+
+class ABC_LLM(ABC):
+    @abstractmethod
+    def create(self, messages: List[LLM_Message]) -> str:
+        raise NotImplementedError()
---
+++ src/service/llm/impl/g4f_llm.py
@@ -0,0 +1,26 @@
+from g4f.client import Client
+from g4f import ChatCompletion
+from src.models.schema.llm_schema import LLM_Message
+from src.service.llm.abc_llm import ABC_LLM
+from typing import List, override
+class G4f_LLM(ABC_LLM):
+    def __init__(self, client = Client()) -> None:
+        self.client = client
+        super().__init__()
+
+    @override
+    def create(self, messages: List[LLM_Message]) -> str:
+        payload = []
+        for m in messages:
+            payload.append({
+                "role": m.role,
+                "content": m.content
+            })
+
+        response: ChatCompletion = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=payload
+        )
+        
+        choice: str = response.choices[0].message.content
+        return choice
