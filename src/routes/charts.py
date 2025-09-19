32602414d2c85facec4b6d01c32ef257c2e55efa
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 15:16:08 -0600

    ♻️ añadir nuevo endpoint para generar esquemas de gráficas

--- src/routes/charts.py
+++ src/routes/charts.py
@@ -1,37 +1,40 @@
 import json
-from fastapi import APIRouter, Query, UploadFile, File
+from typing import List
 
-from src.models.schema.charts import ChartParameter, ChartScheme
-from src.service.chart_builder import handle_parameters
-from src.service.file_handler import generate_df
+from fastapi import APIRouter, Query, UploadFile, File, Depends
+
+from src.models.schema.charts import Chart, ChartSchema
+from src.service.pipe.impl.pandas_pipe import pandas_pipe, PandasPipe
+from src.service.charts.impl.ai_chart_schema import AI_ChartSchema, ai_chart_schema
+from src.service.charts.impl.chart_builder import ChartBuilder, chart_builder_dep
 
 router = APIRouter(prefix="/charts", tags=["Charts"])
 
+
 @router.post(
-    path='/build',
-    description="Construir gráfica a partir de un esquema"
+    path="/generate",
+    description="Generar esquemas simples de gráficas"
 )
-async def build(
-    title:str = Query(description="titulo de la grafica"),
-    chart_type:str = Query(description="tipo de gráfica"),
-    parameter: str = Query(description="Parametros de la gráfica"),
-    file: UploadFile = File(...),
-):
-    # convertir parameter a un Schema
-    data = json.loads(parameter)
-    p = [ChartParameter(**d) for d in  data]
+async def generate_simple_schema(
+        file: UploadFile = File(alias="file"),
+        pipe: PandasPipe = Depends(pandas_pipe),
+        charts: AI_ChartSchema = Depends(ai_chart_schema),
+) -> List[ChartSchema]:
+    df = await pipe.generate_df(file)
+    columns = df.columns.tolist()
+    dtypes = df.dtypes
+    describe = df.describe()
 
-    # validar archivo
-    df = generate_df(file)
+    schema = charts.build_schema(columns, dtypes, describe)
+    return schema
 
-    # generar datos a partir de los parametros otorgados
-    processed_parameters = handle_parameters(p, df)
-    
-    # Crear el esquema de la gráfica
-    chart_scheme = ChartScheme(
-        title=title,
-        chart_type=chart_type,
-        data=processed_parameters
-    )
-    
-    return chart_scheme.model_dump()
+
+@router.post(path='/build', description="Construir gráficas")
+async def build_chart(
+        title: str = Query(description="titulo de la grafica"),
+        chart_type: str = Query(description="tipo de gráfica"),
+        parameter: str = Query(description="parámetros de la gráfica"),
+        file: UploadFile = File(alias="file"),
+        chart: ChartBuilder = Depends(chart_builder_dep),
+) -> Chart:
+    return await chart.build_chart(file, title, chart_type, parameter)
