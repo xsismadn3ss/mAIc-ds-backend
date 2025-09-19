290f37dc19773e1dab8273817d8bda401d927a6f
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-18 19:37:25 -0600

    eliminar esquema no utilizado

--- src/models/schema/ollama.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from pydantic import BaseModel, Field
-from src.config.environment import App_config
-
-class OllamaGenerateRequest(BaseModel):
-    model: str = Field(default=App_config.ai_model)
-    message: str = Field()
-    context: List[int] = Field(default=None)
-    stream: bool = Field(default=True)
