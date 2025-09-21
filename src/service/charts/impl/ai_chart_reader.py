c9824ea9875c236305467a0348d04866c9fb6095
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-20 22:55:04 -0600

    crear servicio para leer graficas con IA y brindar un resumen

---
+++ src/service/charts/impl/ai_chart_reader.py
@@ -0,0 +1,25 @@
+from src.service.llm.impl.g4f_llm import G4f_LLM
+from src.service.llm.abc_llm import ABC_LLM
+from src.config.environment import AppEnv
+from src.models.schema.llm_schema import LLM_Message
+from src.models.schema.charts import Chart
+from typing import List
+
+
+class AI_ChartReader():
+    def __init__(self, llm: ABC_LLM = G4f_LLM()) -> None:
+        self.llm = llm
+        super().__init__()
+
+    
+    def read_charts(self, charts: List[Chart]) -> str:
+        base_prompt: str = AppEnv().read_chart_prompt
+        prompt: str = (
+            f"{base_prompt}\nGráficas: {str([chart.model_dump() for chart in charts])}"
+        )
+        msg = LLM_Message(content=prompt)
+        response = self.llm.create(messages=[msg])
+        return response
+
+def ai_chart_reader_dep() -> AI_ChartReader:
+    return AI_ChartReader()
