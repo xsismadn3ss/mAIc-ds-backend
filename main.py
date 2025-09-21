0b6cd7c138ed4249216a8b6fb6582ff560b76814
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-20 23:10:48 -0600

    aplicar formato usando ruff

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def main():
         reload=True,
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from src.routes import charts
+
 app = FastAPI(title="AI Charts API")
 
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:*', 'http://localhost:5175', 'http://localhost:5173'],
+    allow_origins=[
+        "http://localhost:*",
+        "http://localhost:5175",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,8 +25,5 @@ app.include_router(charts.router)
 async def root():
     return {
         "message": "Bienvenido a AI Charts API!",
-        "routes": [
-            "/charts/generate",
-            "/charts/build"
-        ]
+        "routes": ["/charts/generate", "/charts/build"],
     }
