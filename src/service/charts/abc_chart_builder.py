bc21e90ca647759720348dd12936a143c589660c
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-20 22:41:41 -0600

    actualizar clase abstrac y eliminas dependencias con pandas para reducir responsabilidades

--- src/service/charts/abc_chart_builder.py
+++ src/service/charts/abc_chart_builder.py
@@ -1,13 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Any
-
-from fastapi import UploadFile
+from pandas import DataFrame
 from src.models.schema.charts import Chart
 
 
 class ABC_ChartBuilder(ABC):
     @abstractmethod
-    async def build_chart(
-        self, file: UploadFile, title:str, chart_type:str, parameter: str
+    def build_chart(
+        self, df: DataFrame, title:str, chart_type:str, parameter: str
     ) -> Chart:
         raise NotImplementedError()
